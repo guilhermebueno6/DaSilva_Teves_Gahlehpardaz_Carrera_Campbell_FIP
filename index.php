b112d5aac637cbf35b631322ac3c9d0948042c6d
unknown <guilhermebueno6@gmail.com>
2020-01-18 14:57:28 -0500

    testing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php 
-echo "Testing";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>   Hello This is a H1</h1>
+</body>
+</html>
