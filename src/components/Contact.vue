395bd3a7fe5339970e1831650d62a037580056db
Walter Sebastian Carrera <cwalter1994@gmail.com>
2020-04-12 18:40:09 -0400

    Contact form functional

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -13,27 +13,68 @@
       <form>
         <h2>Don't Hesitate To Contact Us, It's Anonymous.</h2>
         <label for="name">Full Name</label><br>
-        <input type="text" id="name" name="name"><br>
+        <input v-model="name" type="text" id="name" name="name"><br>
 
         <label for="subject">Subject</label><br>
-        <input type="text" id="subject" name="subject"><br>
+        <input v-model="subject" type="text" id="subject" name="subject"><br>
 
         <label for="email">Email</label><br>
-        <input type="text" id="email" name="email"><br>
+        <input v-model="email" type="email" id="email" name="email"><br>
 
         <label for="phone">Phone</label><br>
-        <input type="text" id="phone" name="phone"><br>
+        <input v-model="phone" type="tel" id="phone" name="phone"><br>
 
         <label for="message">Message</label><br>
-        <input type="text" id="message" name="message"><br>
+        <input v-model="message" type="text" id="message" name="message"><br>
 
-        <button type="submit" value="Submit">Submit</button>
+        <div><button type="submit" @click="submit()" value="Submit">Submit</button>
+        <span v-if="formSubmitted">Sent</span></div>
       </form>
     </div>
   </section>
 </template>
 <script>
+import emailjs from 'emailjs-com';
+
 export default {
-  name: 'Contact'
+  name: 'Contact',
+  data () {
+    return {
+      name: null,
+      subject: null,
+      email: null,
+      phone: null,
+      message: null,
+      formSubmitted: false,
+      formInvalid: false
+    }
+  },
+  methods: {
+    submit () {
+      event.preventDefault()
+      if(this.name !== null && this.subject !== null && this.email !== null && this.phone !== null && this.message !== null){
+        this.formInvalid = false
+        this.formSubmitted = false
+        emailjs.init('user_odsIGV4ptj8xmBBPW7S0R')
+        emailjs.send(
+          'default_service',
+          'template_QLNza751',
+          {name: this.name, subject: this.subject, email: this.email, phone: this.phone, message: this.message}
+        ).then((response) => {
+          this.formSubmitted = true
+          console.log('Success', response)
+          this.name = ""
+          this.subject = ""
+          this.email = ""
+          this.phone = "" 
+          this.message = ""
+        }, (error) => {
+          console.log('Failed', error)
+        })
+      } else {
+        this.formInvalid = true
+      }
+    }
+  }
 }
 </script>
